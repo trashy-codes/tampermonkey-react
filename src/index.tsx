53338ca8e5c9708b32470fd95c0975b9ea955c2e
snowlab <snowlab@hotmail.com>
2018-02-24 16:55:22 +0800

    初步成功

--- src/index.tsx
+++ src/index.tsx
@@ -1,14 +1,13 @@
 // ==UserScript==
 // @name ReactTest
-// @description 继承react
+// @description 集成react
 // @version 1.0
 // @author dhswg20077
 // @license MIT
 // @include *
 // @icon http://pics.smotri.com/
 // @run-at document-end
-// @grant none
-// @require https://cdnjs.cloudflare.com/ajax/libs/systemjs/0.20.19/system.js
+// @grant GM_addStyle
 // @require https://unpkg.com/react@16/umd/react.production.min.js
 // @require https://unpkg.com/react-dom@16/umd/react-dom.production.min.js
 // ==/UserScript==
@@ -18,7 +17,37 @@ import * as ReactDOM from 'react-dom';
 import App from './App';
 import './index.css';
 
+function appendPopup(): void {
+  $('body').append('<div id="customPopup"><button id="btnDrag">Drag</button><textarea id="logger" /></div>');
+  // tslint:disable-next-line:max-line-length
+  GM_addStyle('#customPopup{ position:fixed; z-index:99999; top:0;top:50%; left:75%; width:500px; height:500px; background:#8a9bca;}');
+  GM_addStyle('#btnDrag{  width:100%; height:20px; background:#8a9bca; cursor:move;}');
+  GM_addStyle('#logger{  width:98.8%; height:80%; }');
+
+  // tslint:disable-next-line:typedef
+  $('#btnDrag').mousedown(function (e) {
+    let iDiffX: number = e.pageX - ($(this).offset() as JQuery.Coordinates).left;
+    var iDiffY: number = e.pageY - ($(this).offset() as JQuery.Coordinates).top;
+
+    let tmpScrollTop: number | undefined = $(window.document).scrollTop();
+    let scrollTop: number = tmpScrollTop ? tmpScrollTop : 0;
+    // tslint:disable-next-line:no-shadowed-variable typedef
+    $(document).mousemove(function (e) {
+      $('#customPopup').css({ 'left': (e.pageX - iDiffX), 'top': (e.pageY - scrollTop - iDiffY) });
+    });
+  });
+  // tslint:disable-next-line:typedef
+  $('#customPopup').mouseup(function () {
+    $(document).unbind('mousemove');
+  });
+}
+
+(function () {
+  'use strict';
+  appendPopup();
+})();
+
 ReactDOM.render(
-  <App />,
-  document.getElementById('root') as HTMLElement
+  < App />,
+  document.getElementById('customPopup') as HTMLElement
 );
