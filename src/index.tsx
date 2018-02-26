65d37c4c02ee2725d8a757a4c4de91098814a34e
snowlab <snowlab@hotmail.com>
2018-02-26 17:15:12 +0800

    合并css

--- src/index.tsx
+++ src/index.tsx
@@ -16,13 +16,14 @@ import * as React from 'react';
 import * as ReactDOM from 'react-dom';
 import App from './App';
 import './index.css';
+import * as $ from 'jquery';
 
 function appendPopup(): void {
   $('body').append('<div id="customPopup"><button id="btnDrag">Drag</button><textarea id="logger" /></div>');
   // tslint:disable-next-line:max-line-length
-  GM_addStyle('#customPopup{ position:fixed; z-index:99999; top:0;top:50%; left:75%; width:500px; height:500px; background:#8a9bca;}');
-  GM_addStyle('#btnDrag{  width:100%; height:20px; background:#8a9bca; cursor:move;}');
-  GM_addStyle('#logger{  width:98.8%; height:80%; }');
+  // GM_addStyle('#customPopup{ position:fixed; z-index:99999; top:0;top:50%; left:75%; width:500px; height:500px; background:#8a9bca;}');
+  // GM_addStyle('#btnDrag{  width:100%; height:20px; background:#8a9bca; cursor:move;}');
+  // GM_addStyle('#logger{  width:98.8%; height:80%; }');
 
   // tslint:disable-next-line:typedef
   $('#btnDrag').mousedown(function (e) {
