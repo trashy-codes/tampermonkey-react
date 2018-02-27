ef5647b2216790eaab6303eadcdb862f5b8aba23
snowlab <snowlab@hotmail.com>
2018-02-27 14:45:03 +0800

    集成完成

--- src/App.tsx
+++ src/App.tsx
@@ -2,14 +2,45 @@ import * as React from 'react';
 import './App.css';
 import 'antd/dist/antd.css';
 // babel-plugin-import 会帮助你加载 JS 和 CSS
-import { DatePicker } from 'antd';
+import { Row, Col, Button } from 'antd';
+import * as $ from 'jquery';
 
 // const logo = require('./logo.svg');
 
 class App extends React.Component {
+  constructor(props: {}) {
+    super(props);
+  }
+  // tslint:disable-next-line:no-any
+  TitleMouseDown(e: React.MouseEvent<Button>) {
+    let iDiffX: number = e.pageX - ($(e.currentTarget).offset() as JQuery.Coordinates).left;
+    var iDiffY: number = e.pageY - ($(e.currentTarget).offset() as JQuery.Coordinates).top;
+
+    let tmpScrollTop: number | undefined = $(window.document).scrollTop();
+    let scrollTop: number = tmpScrollTop ? tmpScrollTop : 0;
+    // tslint:disable-next-line:no-shadowed-variable typedef
+    $(document).mousemove(function (e) {
+      $('#customPopup').css({ 'left': (e.pageX - iDiffX), 'top': (e.pageY - scrollTop - iDiffY) });
+    });
+  }
+  TitleMouseUP(e: React.FormEvent<HTMLElement>) {
+    $(document).unbind('mousemove');
+  }
   render() {
     return (
-      <DatePicker />
+      <div>
+        <Row>
+          <Col span={24}>
+            <Button onMouseUp={this.TitleMouseUP} onMouseDown={this.TitleMouseDown} style={{ width: '100%' }} type="primary">
+              Title</Button>
+          </Col>
+        </Row>
+        <Row >
+          <Col span={24}>
+            <Button type="primary">NewPage</Button>
+          </Col>
+        </Row>
+      </div >
     );
   }
 }
--- src/index.tsx
+++ src/index.tsx
@@ -19,28 +19,10 @@ import './index.css';
 import * as $ from 'jquery';
 
 function appendPopup(): void {
-  $('body').append('<div id="customPopup"><button id="btnDrag">Drag</button><textarea id="logger" /></div>');
-  // tslint:disable-next-line:max-line-length
+  $('body').append('<div id="customPopup"></div>');
   // GM_addStyle('#customPopup{ position:fixed; z-index:99999; top:0;top:50%; left:75%; width:500px; height:500px; background:#8a9bca;}');
   // GM_addStyle('#btnDrag{  width:100%; height:20px; background:#8a9bca; cursor:move;}');
   // GM_addStyle('#logger{  width:98.8%; height:80%; }');
-
-  // tslint:disable-next-line:typedef
-  $('#btnDrag').mousedown(function (e) {
-    let iDiffX: number = e.pageX - ($(this).offset() as JQuery.Coordinates).left;
-    var iDiffY: number = e.pageY - ($(this).offset() as JQuery.Coordinates).top;
-
-    let tmpScrollTop: number | undefined = $(window.document).scrollTop();
-    let scrollTop: number = tmpScrollTop ? tmpScrollTop : 0;
-    // tslint:disable-next-line:no-shadowed-variable typedef
-    $(document).mousemove(function (e) {
-      $('#customPopup').css({ 'left': (e.pageX - iDiffX), 'top': (e.pageY - scrollTop - iDiffY) });
-    });
-  });
-  // tslint:disable-next-line:typedef
-  $('#customPopup').mouseup(function () {
-    $(document).unbind('mousemove');
-  });
 }
 
 (function () {
